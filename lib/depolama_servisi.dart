00389d276b87ed27f43c43ffdafb7992ab93d058
dest-os <destosares@gmail.com>
2026-08-18 16:09:23 +0300

    Update depolama_servisi.dart

--- lib/depolama_servisi.dart
+++ lib/depolama_servisi.dart
@@ -7,10 +7,12 @@ class DepolamaServisi {
   static final _storage = const FlutterSecureStorage();
   static SharedPreferences? _prefs;
 
+  // Servis Başlatma (main.dart için)
   static Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
   }
 
+  // Kullanıcı Adı İşlemleri
   static Future<void> kaydetKullaniciAdi(String kullaniciAdi) async {
     _prefs ??= await SharedPreferences.getInstance();
     await _prefs?.setString('kullaniciAdi', kullaniciAdi);
@@ -21,13 +23,23 @@ class DepolamaServisi {
     return _prefs?.getString('kullaniciAdi');
   }
 
+  // API Bilgisi İşlemleri
   static Future<ApiBilgisi?> apiBilgisiGetir() async {
     _prefs ??= await SharedPreferences.getInstance();
     final url = _prefs?.getString('apiAdresi') ?? '';
     final id = _prefs?.getString('apiId') ?? 'default_id';
-    return ApiBilgisi(id: id, apiAdresi: url);
+    final firma = _prefs?.getString('firmaAdi') ?? 'Varsayılan Firma';
+    return ApiBilgisi(id: id, apiAdresi: url, firmaAdi: firma);
   }
 
+  static Future<void> apiBilgisiKaydet(ApiBilgisi api) async {
+    _prefs ??= await SharedPreferences.getInstance();
+    await _prefs?.setString('apiId', api.id);
+    await _prefs?.setString('apiAdresi', api.apiAdresi);
+    await _prefs?.setString('firmaAdi', api.firmaAdi);
+  }
+
+  // Oturum İşlemleri (Güvenli Depolama)
   Future<void> oturumAyarla(String kullaniciAdi, String sifre) async {
     await _storage.write(
       key: 'oturum',
@@ -38,12 +50,13 @@ class DepolamaServisi {
   Future<Map<String, dynamic>?> oturumBilgisiAl() async {
     final json = await _storage.read(key: 'oturum');
     if (json != null) {
-      return jsonDecode(json);
+      return jsonDecode(json) as Map<String, dynamic>;
     } else {
       return null;
     }
   }
 
+  // Uygulama Ayarları
   Future<void> ayarlarKaydet(String ayarlar) async {
     _prefs ??= await SharedPreferences.getInstance();
     await _prefs?.setString('ayarlar', ayarlar);
@@ -54,6 +67,7 @@ class DepolamaServisi {
     return _prefs?.getString('ayarlar');
   }
 
+  // Kamera Ayarları
   Future<void> kameraAlgila(bool algila) async {
     _prefs ??= await SharedPreferences.getInstance();
     await _prefs?.setBool('kameraAlgila', algila);
@@ -64,6 +78,7 @@ class DepolamaServisi {
     return _prefs?.getBool('kameraAlgila');
   }
 
+  // Ses Ayarları
   Future<void> sesDegistir(String ses) async {
     _prefs ??= await SharedPreferences.getInstance();
     await _prefs?.setString('ses', ses);
@@ -74,6 +89,7 @@ class DepolamaServisi {
     return _prefs?.getString('ses');
   }
 
+  // Akıllı Eğitim ve Menü Ayarları
   Future<void> akilliEgitimKaydet(String egitim) async {
     _prefs ??= await SharedPreferences.getInstance();
     await _prefs?.setString('akilliEgitim', egitim);
@@ -94,6 +110,7 @@ class DepolamaServisi {
     return _prefs?.getString('butonMenu');
   }
 
+  // Jarvis Sesli Uyandırma
   Future<void> jarvisSesliUyandirmaKaydet(bool uyandirma) async {
     _prefs ??= await SharedPreferences.getInstance();
     await _prefs?.setBool('jarvisSesliUyandirma', uyandirma);
