a068f2573dfbfe4ddce85b99da46cd435f6bfad7
dest-os <destosares@gmail.com>
2026-08-21 20:54:51 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'giris_ekrani.dart';
 import 'sohbet_ekrani.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // ARES JARVIS Arayüzü İçin Yatay Ekran ve Tam Ekran Kilitlemesi
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+
+  // Durum çubuklarını gizleyip tam ekran siberpunk deneyimi sağlama
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
   final prefs = await SharedPreferences.getInstance();
-  
   final String? kullaniciAdi = prefs.getString('kullanici_adi');
 
   runApp(AresAsistanApp(
@@ -24,10 +34,11 @@ class AresAsistanApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Ares Asistan',
+      title: 'Ares JARVIS Asistan',
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: Colors.black,
+        fontFamily: 'Roboto',
       ),
       home: baslangicEkrani,
     );
